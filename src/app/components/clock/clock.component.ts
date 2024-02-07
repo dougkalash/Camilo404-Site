d0f29bec8ce3408d9740acc8f0b6df5ae56b04f0
DouG <hudsondouglas22@gmail.com>
2024-02-07 18:19:02 -0300

    Update

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -9,30 +9,30 @@ export class ClockComponent implements OnInit {
 
   strTime: string = '';
 
-  constructor() {
-
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.updateTime();
-    setInterval(() => this.updateTime(), 1000);
+    // Comentado para desativar a atualização automática do relógio
+    // this.updateTime();
+    // setInterval(() => this.updateTime(), 1000);
   }
 
+  // Função comentada para preservar a lógica de atualização do tempo, mas desativada por enquanto
+  /*
   updateTime() {
-    // Obtener la hora local
     const date = new Date();
     let hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
 
-    // Convertir a formato de 12 horas
     const ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
-    hours = hours ? hours : 12; // La hora '0' debe ser '12'
+    hours = hours ? hours : 12; // A hora '0' deve ser '12'
     const formattedHours = hours < 10 ? '0' + hours : hours;
     const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
     const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
 
     this.strTime = formattedHours + ':' + formattedMinutes + ':' + formattedSeconds + ' ' + ampm;
   }
+  */
 }
